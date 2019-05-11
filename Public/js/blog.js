7a6165a6bccf472110f6a19389e504c8bbcafbb1
Blacksheep <1247740650@qq.com>
2019-05-12 00:58:01 +0800

    '页面布局调整'

--- Public/js/blog.js
+++ Public/js/blog.js
@@ -4,89 +4,57 @@
 $(function () {
     /**********监听窗口大小变化进行布局调整***********/
     windowSize();
-    function windowSize(){
-        let windowWid=$(window).width();
-        if(windowWid<990){
+
+    function windowSize() {
+        let windowWid = $(window).width();
+        if (windowWid < 990) {
             $("#rightBarBlock").hide();
         }
-        if(windowWid>990){
+        if (windowWid > 990) {
             $("#rightBarBlock").show();
         }
-        var imgWidth= $('.blogImg').width()*0.8;
-        $(".blogCon").css({
-            height:imgWidth
-        });
-        $(".blogImg").css({
-            height:imgWidth
-        });
-        $(".blogImg>img").css({
-            height:imgWidth
-        })
-        $(".blogArt").css({
-            height:imgWidth
-        })
-
     }
+
     $(window).resize(function () {          //当浏览器大小变化时
         windowSize()
     });
 
 
-
     /**********监听滚动条***********/
 
-    $(window).scroll(function(event){
-        let windowHei=$(window).height();
-        var morebtn=$("#morebtn").offset().top- document.documentElement.scrollTop-windowHei;
-        if(morebtn<100&&morebtn>0){
-            moreBtn();
-        }
-        if(document.documentElement.scrollTop>=600){
+    $(window).scroll(function (event) {
+        let windowHei = $(window).height();
+        if (document.documentElement.scrollTop >= 600) {
             $(".Up").show();
-        }else {
+        } else {
             $(".Up").hide();
         }
     });
 
 
-
-        /*********回到顶部****/
-    $(".Up").click(function(){
-        $('.navbar').css({
-            backgroundColor: "#222",
-            borderColor: " #080808",
-            position: "relative",
-            minHeight: "50px",
-            marginBottom: " 20px",
-            border: "1px solid transparent",
-        })
-        $('#content').css({
-            position:"absolute",
-            top:'70px',
-            width:'100%',
-            zIndex:1
-        });
-        var morebtn=$("body").offset().top- document.documentElement.scrollTop;
-       let sudu= document.documentElement.scrollTop*0.05;
-        var timer=setInterval(function(){
-            document.documentElement.scrollTop=document.documentElement.scrollTop-sudu;
-            if(document.documentElement.scrollTop<=0){
-                document.documentElement.scrollTop=0;
+    /*********回到顶部****/
+    $(".Up").click(function () {
+        var morebtn = $("body").offset().top - document.documentElement.scrollTop;
+        let sudu = document.documentElement.scrollTop * 0.05;
+        var timer = setInterval(function () {
+            document.documentElement.scrollTop = document.documentElement.scrollTop - sudu;
+            if (document.documentElement.scrollTop <= 0) {
+                document.documentElement.scrollTop = 0;
                 clearInterval(timer);
             }
-        },10);
+        }, 10);
     });
 
 
-     /**********获取文章***********/
-    getBloglist(0,true);
-    function getBloglist(Num,news) {
+    /**********获取文章***********/
+    getBloglist(0, true);
+    function getBloglist(Num, news) {
         let start = Num;
         let firstTime = {
             start: start,
             end: 10
         };
-        if(news){
+        if (news) {
             $('.blogBarBlock').html("");
         }
         $.ajax({
@@ -107,14 +75,14 @@ $(function () {
 
                 for (let i = 0; i < data.length; i++) {
                     var imgReg = /<img src="https:\/\/wupopo-1256296697.cos.ap-chengdu.myqcloud.com.*?(?:>|\/>)/gi;
-                    var imgarr=data[i].content.match(imgReg); //查出所有图片地址
-                    var str=data[i].content.replace(imgReg,'[图片]');
+                    var imgarr = data[i].content.match(imgReg); //查出所有图片地址
+                    var str = data[i].content.replace(imgReg, '[图片]');
                     var imgsrc;
-                  if(imgarr==null){
-                    imgsrc='blog/img/demoimg.jpg'
-                  }else {
-                      imgsrc=imgarr[0].match(/myqcloud.com\/(\S*)"/)[1];
-                  }
+                    if (imgarr == null) {
+                        imgsrc = 'blog/img/demoimg.jpg'
+                    } else {
+                        imgsrc = imgarr[0].match(/myqcloud.com\/(\S*)"/)[1];
+                    }
 
 
                     let likearr = []
@@ -133,25 +101,29 @@ $(function () {
 
                     $('#blogBarBlock').append(`
                         <div class="blogBar">
-                            <div class="Bar">
+                            <div class="Bar" style="background-image: url('https://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/` + imgsrc + `')">
                                 <div class="senderInfo">
-                                <img src="http://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/blog/img/` + data[i].img + `" alt="">
-                                <a class="sendername" href="../home/` + data[i].sendername + `">` + data[i].name + `</a>
-                                <p class="title">《 <a href="../blog/` + data[i].id + `" title="` + data[i].title + `" name="` + data[i].id + `">` + data[i].title + `</a> 》</p>
-                             <i class="sendTime">` + data[i].time + `</i>
+                                    <img src="http://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/blog/img/` + data[i].img + `" alt="">
+                                    <p>
+                                        <b class="title"><a class="ti" href="../blog/` + data[i].id + `" title="` + data[i].title + `" name="` + data[i].id + `">` +
+                                        data[i].title + `</a></b>
+                                    </p>
+                                     <p>
+                                        <b class="sendername">作者：</b><a class="sendername" href="../home/` + data[i].sendername + `">` + data[i].name + `</a>
+                                     </p>
+                                      <p>
+                                        <b class="sendtime">发表时间：</b>` + data[i].time + `
+                                     </p>
+                                </div>
                             </div>
-                            <div class="blogCon">
-                            <div class="blogImg">
-                                <img src="https://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/`+imgsrc+`" alt="">
-                            </div>
-                            <div class="blogArt">
+                            <div class="blogCon">                 
+                                <div class="blogArt">
                                  ` + str + `
-                            </div>
-
+                                </div>
                             </div>
                             <div class="blogBarFooter">
-                                <a class="readAll" href="../blog/` + data[i].id + `">查看全文</a>
-                                <p class="likeBtn glyphicon glyphicon-heart ` + likebtn + `" ><i class="Lnum">` + likearr.length + `</i></p>
+                                    <a class="readAll" href="../blog/` + data[i].id + `">查看全文</a>
+                                    <p class="likeBtn glyphicon glyphicon-heart ` + likebtn + `" ><i class="Lnum">` + likearr.length + `</i></p>
                             </div>
                             </div>
                         </div>
@@ -162,17 +134,19 @@ $(function () {
     }
 
     var scd = 10;
-    function moreBtn(){
-        getBloglist(scd,false);
+
+    function moreBtn() {
+        getBloglist(scd, false);
         scd = scd + 10;
     }
+
     $("#more").click(morebtn);
 
 
-    $('body').on("click",".like",function(){
+    $('body').on("click", ".like", function () {
         let href = this.previousSibling.previousSibling.getAttribute("href");
         let blogid = href.match(/blog\/(.*)/)[1];
-        let _this=this;
+        let _this = this;
         $.ajax({
             type: 'GET',
             url: "/like",
@@ -184,17 +158,17 @@ $(function () {
                 tip(err.responseJSON.msg);
             },
             success: function (data) {
-                let num=Number(_this.firstChild.innerHTML);
-                _this.firstChild.innerHTML=num -1
-               _this.classList.remove('like');
+                let num = Number(_this.firstChild.innerHTML);
+                _this.firstChild.innerHTML = num - 1
+                _this.classList.remove('like');
                 _this.classList.add('unlike');
             }
         });
     });
-    $('body').on("click",".unlike",function(){
+    $('body').on("click", ".unlike", function () {
         let href = this.previousSibling.previousSibling.getAttribute("href");
         let blogid = href.match(/blog\/(.*)/)[1];
-        let _this=this;
+        let _this = this;
         $.ajax({
             type: 'GET',
             url: "/like",
@@ -206,8 +180,8 @@ $(function () {
                 tip(err.responseJSON.msg);
             },
             success: function (data) {
-                let num=Number(_this.firstChild.innerHTML);
-                _this.firstChild.innerHTML=num+1;
+                let num = Number(_this.firstChild.innerHTML);
+                _this.firstChild.innerHTML = num + 1;
                 _this.classList.remove('unlike');
                 _this.classList.add('like');
             }
@@ -217,69 +191,69 @@ $(function () {
 
 
     /*****文章搜索*****/
-            //搜索主函数
+    //搜索主函数
 
-            //点击事件绑定
-        $(".seabtn").click(function(){
+    //点击事件绑定
+    $(".seabtn").click(function () {
 
-            var seaCon=$(".seainp").val();
-            $.ajax({
-                type:'POST',
-                url:'/search',
-                data:{
-                    content:seaCon
-                },
-                error:function(err){
-                    alert(err.responseJSON.msg)
-                },
-                success:function(reslut){
-                    $("#more").hide();
-                    $('#blogBarBlock').html("");
-                   if(reslut.data.length==0){
-                       $('#blogBarBlock').append(`
+        var seaCon = $(".seainp").val();
+        $.ajax({
+            type: 'POST',
+            url: '/search',
+            data: {
+                content: seaCon
+            },
+            error: function (err) {
+                alert(err.responseJSON.msg)
+            },
+            success: function (reslut) {
+                $("#more").hide();
+                $('#blogBarBlock').html("");
+                if (reslut.data.length == 0) {
+                    $('#blogBarBlock').append(`
                         <p class="tipsinfo">没有你要的信息，建议你输入存在且连续的部分信息</p>
                         <a href="/blog" class="glyphicon glyphicon-menu-left tipsback">返回</a>
                        `);
-                   }else {
-                       $('#blogBarBlock').append(`
+                } else {
+                    $('#blogBarBlock').append(`
                         <p class="tipsinfo">你可能要找以下信息</p>
                         <a href="/blog" class="glyphicon glyphicon-menu-left tipsback">返回</a>
                        `);
-                       let data = reslut.data;
-                       if (data.length == 10) {
-                           $("#morebtn").show();
-                       } else {
-                           $("#morebtn").hide();
-                       }
-                       let username = reslut.msg;
+                    let data = reslut.data;
+                    if (data.length == 10) {
+                        $("#morebtn").show();
+                    } else {
+                        $("#morebtn").hide();
+                    }
+                    let username = reslut.msg;
 
-                       for (let i = 0; i < data.length; i++) {
-                           var imgReg = /<img src="https:\/\/wupopo-1256296697.cos.ap-chengdu.myqcloud.com.*?(?:>|\/>)/gi;
-                           var imgarr=data[i].content.match(imgReg); //查出所有图片地址
-                           var str=data[i].content.replace(imgReg,'[图片]');
-                           var imgsrc;
-                           if(imgarr==null){
-                               imgsrc='blog/img/demoimg.jpg'
-                           }else {
-                               imgsrc=imgarr[0].match(/myqcloud.com\/(\S*)"/)[1];
-                           }
+                    for (let i = 0; i < data.length; i++) {
+                        var imgReg = /<img src="https:\/\/wupopo-1256296697.cos.ap-chengdu.myqcloud.com.*?(?:>|\/>)/gi;
+                        var imgarr = data[i].content.match(imgReg); //查出所有图片地址
+                        var str = data[i].content.replace(imgReg, '[图片]');
+                        var imgsrc;
+                        if (imgarr == null) {
+                            imgsrc = 'blog/img/demoimg.jpg'
+                        } else {
+                            imgsrc = imgarr[0].match(/myqcloud.com\/(\S*)"/)[1];
+                        }
 
 
-                           let likearr = []
-                           if (data[i].likes) {
-                               likearr = data[i].likes.split('|');
-                           }
+                        let likearr = []
+                        if (data[i].likes) {
+                            likearr = data[i].likes.split('|');
+                        }
 
-                           var likebtn;
-                           if (username == 'nologin') {
-                               likebtn = 'unlike'
-                           } else if (likearr.indexOf(username) > -1) {
-                               likebtn = 'like'
-                           } else {
-                               likebtn = 'unlike'
-                           }
+                        var likebtn;
+                        if (username == 'nologin') {
+                            likebtn = 'unlike'
+                        } else if (likearr.indexOf(username) > -1) {
+                            likebtn = 'like'
+                        } else {
+                            likebtn = 'unlike'
+                        }
 
-                           $('#blogBarBlock').append(`
+                        $('#blogBarBlock').append(`
                         <div class="blogBar">
                             <div class="Bar">
                                 <div class="senderInfo">
@@ -290,7 +264,7 @@ $(function () {
                             </div>
                             <div class="blogCon">
                             <div class="blogImg">
-                                <img src="https://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/`+imgsrc+`" alt="">
+                                <img src="https://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/` + imgsrc + `" alt="">
                             </div>
                             <div class="blogArt">
                                  ` + str + `
@@ -303,12 +277,11 @@ $(function () {
                             </div>
                         </div>
                     `);
-                       }
-                   }
+                    }
                 }
-            })
-        });
-
+            }
+        })
+    });
 
 
     //文本编辑器配置
@@ -363,7 +336,7 @@ $(function () {
                 $('nav').show();
                 $("#content").show();
                 $(".blogBar").remove();
-                getBloglist(0,true)
+                getBloglist(0, true)
             }
         });
 
@@ -385,11 +358,11 @@ $(function () {
     }
 
 
-    function tip(str){   //tips
+    function tip(str) {   //tips
         $(".tips").show().text(str);
-        setTimeout(function(){
+        setTimeout(function () {
             $(".tips").hide();
-        },2000);
+        }, 2000);
 
     }
 });
--- Public/js/common.js
+++ Public/js/common.js
@@ -1,212 +1,219 @@
 /**
  * Created by asus on 2019/3/24.
  */
-$(function(){
+$(function () {
 
 
+    //监听页面滚动
 
-	//监听页面滚动
+    function htmlUp() {
+        $('.navbar').css({
+            backgroundColor: "#ffffff",
+            position: "fixed",
+            width: '100%',
+            top: "0"
+        });
+        var content = document.documentElement.scrollTop;
+        if (content < 50) {
+            $('.navbar').css({
+                backgroundColor: "#ffffff",
+                borderColor: " #080808",
+                position: "relative",
+                minHeight: "50px",
+                marginBottom: " 20px",
+                border: "1px solid transparent"
+            });
+        }
+    }
 
-	function htmlUp(){
-		$('.navbar').css({
-			backgroundColor: "#ffffff",
-			position:"fixed",
-			width:'100%',
-			top:"0"
-		});
-		var content=document.documentElement.scrollTop;
-		if(content<50){
-			$('.navbar').css({
-				backgroundColor: "#ffffff",
-				borderColor: " #080808",
-				position: "relative",
-				minHeight: "50px",
-				marginBottom: " 20px",
-				border: "1px solid transparent"
-			});
-		}
-	}
-	function  htmlDown(){
-		$('.navbar').css({
-			backgroundColor: "#ffffff",
-			borderColor: " #080808",
-			position: "relative",
-			minHeight: "50px",
-			marginBottom: " 20px",
-			border: "1px solid transparent"
-		});
-		//$('#content').css({
-		//	position: "absolute",
-		//	top: '70px',
-		//	width: '100%',
-		//	zIndex: 1
-		//});
-	}
-	var scrollFunc = function (e) {
-		e = e || window.event;
-		if (e.wheelDelta) {  //第一步：先判断浏览器IE，谷歌滑轮事件
-			if (e.wheelDelta > 0) { //当滑轮向上滚动时
+    function htmlDown() {
+        $('.navbar').css({
+            backgroundColor: "#ffffff",
+            borderColor: " #080808",
+            position: "relative",
+            minHeight: "50px",
+            marginBottom: " 20px",
+            border: "1px solid transparent"
+        });
+        //$('#content').css({
+        //	position: "absolute",
+        //	top: '70px',
+        //	width: '100%',
+        //	zIndex: 1
+        //});
+    }
 
-			/*	htmlUp()*/
-			}
-			if (e.wheelDelta < 0) { //当滑轮向下滚动时
-				/*htmlDown()*/
-			}
-		} else if (e.detail) {  //Firefox滑轮事件
-			if (e.detail> 0) { //当滑轮向上滚动时
-			/*	htmlUp()*/
-			}
-			if (e.detail< 0) { //当滑轮向下滚动时
-				/*htmlDown()*/
-			}
-		}
-	}
-	//给页面绑定滑轮滚动事件
-	if (document.addEventListener) {//firefox
-		document.addEventListener('DOMMouseScroll', scrollFunc, false);
-	}
-	//滚动滑轮触发scrollFunc方法  //ie 谷歌
-	window.onmousewheel = document.onmousewheel = scrollFunc;
+    var scrollFunc = function (e) {
+        e = e || window.event;
+        if (e.wheelDelta) {  //第一步：先判断浏览器IE，谷歌滑轮事件
+            if (e.wheelDelta > 0) { //当滑轮向上滚动时
 
-	$.ajax({
-		type:"GET",
-		url:'/pageConfig',
-		data:{
-			html_name:"index"
-		},
-		error:function(err){
-			console.log(err);
-		},
-		success:function(data){
-			console.log(data);
-			$(".panel-default>.panel-heading").css({
-				backgroundColor:data.nav_color
-			});
-			/*if(data.background_img){
-				$("#content").css({
-					backgroundImage:"url("+data.background_img+")",
-					minHeight: $(window).height()-50
-				});
-			}*/
-		}
-	})
+                /*	htmlUp()*/
+            }
+            if (e.wheelDelta < 0) { //当滑轮向下滚动时
+                /*htmlDown()*/
+            }
+        } else if (e.detail) {  //Firefox滑轮事件
+            if (e.detail > 0) { //当滑轮向上滚动时
+                /*	htmlUp()*/
+            }
+            if (e.detail < 0) { //当滑轮向下滚动时
+                /*htmlDown()*/
+            }
+        }
+    }
+    //给页面绑定滑轮滚动事件
+    if (document.addEventListener) {//firefox
+        document.addEventListener('DOMMouseScroll', scrollFunc, false);
+    }
+    //滚动滑轮触发scrollFunc方法  //ie 谷歌
+    window.onmousewheel = document.onmousewheel = scrollFunc;
 
-    $('#exit').click(function(){
-        $.get('/exit',{
-            role:"user"
-        },function(data){
-            if(data=="ok"){
-                window.location.href=window.location.href;
+    $.ajax({
+        type: "GET",
+        url: '/pageConfig',
+        data: {
+            html_name: "index"
+        },
+        error: function (err) {
+            console.log(err);
+        },
+        success: function (data) {
+            let blog = data.hotblog;
+            var imgReg = /<img src="https:\/\/wupopo-1256296697.cos.ap-chengdu.myqcloud.com.*?(?:>|\/>)/gi;
+            var imgarr = blog.content.match(imgReg); //查出所有图片地址
+            var str = blog.content.replace(imgReg, '[图片]');
+            var imgsrc;
+            if (imgarr == null) {
+                imgsrc = 'https://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/' + 'blog/img/demoimg.jpg'
+            } else {
+                imgsrc = 'https://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/' + imgarr[0].match(/myqcloud.com\/(\S*)"/)[1];
+            }
+            $("#hotHeader").css("backgroundImage","url(" + imgsrc + ")");
+            $(".index_title").attr('href', 'blog/' + blog.id);
+            $(".index_title b").text(blog.title)
+            $(".index_stime b").text(blog.time)
+            $(".index_sendername").attr('href', 'home/' + blog.sendername).text(blog.name);
+            $(".readmore").attr('href', 'blog/' + blog.id);
+            $('.commbtn').attr('href', 'blog/' + blog.id + "#sendinp");
+            $('.blogcontent').html(str);
+        }
+    })
+
+    $('#exit').click(function () {
+        $.get('/exit', {
+            role: "user"
+        }, function (data) {
+            if (data == "ok") {
+                window.location.href = window.location.href;
             }
         });
     });
-	
-	
-	/*Vue.component('like',{
-		props:["commid","style"],
-		template:`<div class='like_panel'>
-			<span class='glyphicon glyphicon-heart v_likebtn' @click='onclick()'></span>
-		</div>`,
-		methods:{
-			onclick:function(){
-				alert(this.commid);
-			}
-		}
-	});*/
-
-	/*****判断用户状态*****/
-	islogin();
-	function islogin(){
-		$.ajax({
-			type: 'GET',
-			url: "../isLogin",
-			success: (data)=> {
-				if (data.islogin) {
-					getmsg(data.username)
-				}else {
-					console.log("未登录")
-				}
-			}
-
-		})
-	}
-	const timer=setInterval(islogin,30000);
-	const getmsg = (username)=> {
-
-		$.ajax({
-			type:"POST",
-			url:"../getmsg",
-			data:{
-				msg_tofrom_username:username
-			},
-			error:(err)=>{
-				console.log(err);
-			},
-			success:(data)=>{
-				$(".badge").remove();
-				$(".lists").html("");
-				let msgcount=data.length;
-				if(data.length==0){
-					$(".badge").remove();
-					$(".AllreadD").hide();
-				}else {
-					$(".msg").append(`<span class="badge">`+msgcount+`</span>`);
-					$(".AllreadD").show();
-				}
-				$(".unread").text("未读消息（"+msgcount+"）");
-				console.log(data)
-				for(let i=0;i<data.length;i++){
-					let dos;
-					let typex;
-					let content;
-					if(data[i].parent_type=="blog"){
-						dos="评论了你的文章:";
-						typex='去往原文章';
-						content="“"+data[i].content+"”";
-					}else if(data[i].parent_type=="comm"){
-						dos="回复了你的的评论：";
-						typex='去往原文章';
-						content="“"+data[i].content+"”";
-					}/*else if(data[i].parent_type=="good"){
-						dos="赞了你名为";
-						typex='的文章';
-						content="《"+data[i].msg_content+"》";
-					}*/
 
 
-					let index=i+1;
-					$(".lists").append(`
+    /*Vue.component('like',{
+        props:["commid","style"],
+        template:`<div class='like_panel'>
+            <span class='glyphicon glyphicon-heart v_likebtn' @click='onclick()'></span>
+        </div>`,
+        methods:{
+            onclick:function(){
+                alert(this.commid);
+            }
+        }
+    });*/
+
+    /*****判断用户状态*****/
+    islogin();
+
+    function islogin() {
+        $.ajax({
+            type: 'GET',
+            url: "../isLogin",
+            success: (data) => {
+                if (data.islogin) {
+                    getmsg(data.username)
+                } else {
+
+                }
+            }
+
+        })
+    }
+
+    const timer = setInterval(islogin, 30000);
+    const getmsg = (username) => {
+
+        $.ajax({
+            type: "POST",
+            url: "../getmsg",
+            data: {
+                msg_tofrom_username: username
+            },
+            error: (err) => {
+                console.log(err);
+            },
+            success: (data) => {
+                $(".badge").remove();
+                $(".lists").html("");
+                let msgcount = data.length;
+                if (data.length == 0) {
+                    $(".badge").remove();
+                    $(".AllreadD").hide();
+                } else {
+                    $(".msg").append(`<span class="badge">` + msgcount + `</span>`);
+                    $(".AllreadD").show();
+                }
+                $(".unread").text("未读消息（" + msgcount + "）");
+                for (let i = 0; i < data.length; i++) {
+                    let dos;
+                    let typex;
+                    let content;
+                    if (data[i].parent_type == "blog") {
+                        dos = "评论了你的文章:";
+                        typex = '去往原文章';
+                        content = "“" + data[i].content + "”";
+                    } else if (data[i].parent_type == "comm") {
+                        dos = "回复了你的的评论：";
+                        typex = '去往原文章';
+                        content = "“" + data[i].content + "”";
+                    }
+
+
+                    let index = i + 1;
+                    $(".lists").append(`
 								<i class="msgli">
-                                    <span class="indexs">`+index+`</span>
-                                    <img src="http://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/blog/img/`+data[i].img+`" class="whoimg" alt="">
-                                    <a href="../home/`+data[i].target_username+`" class="who">`+data[i].name+`</a>
-                                    <p class="do">`+dos+`</p>
-                                    <p class="ctitle">`+content+`</p>
-                                    <a  href="../blog/`+data[i].ancestors_id+`" class="typex">`+typex+`</a>
+                                    <span class="indexs">` + index + `</span>
+                                    <img src="http://wupopo-1256296697.cos.ap-chengdu.myqcloud.com/blog/img/` + data[i].img + `" class="whoimg" alt="">
+                                    <a href="../home/` + data[i].target_username + `" class="who">` + data[i].name + `</a>
+                                    <p class="do">` + dos + `</p>
+                                    <p class="ctitle">` + content + `</p>
+                                    <a  href="../blog/` + data[i].ancestors_id + `" class="typex">` + typex + `</a>
                                 </i>
 					`);
-				}
-			}
+                }
+            }
 
-		})
-	}
+        })
+    }
 
-	$.ajax({
-		type:"POST",
-		url:"../getSidebar",
-		error:function(err){
-			alert(err.responeJSON.msg);
-		},
-		success:function(data){
-			$(".noticeBody").text(data.data[0].tips);
-			$(".vlogBody").attr("src",data.data[0].vlog);
-			let obj=data.data[0].recommended;
-			for (let key in obj) {
-				$(".recBody").append(`
-					<li><a href="`+obj[key]+`">`+key+`</a></li>
+    $.ajax({
+        type: "POST",
+        url: "../getSidebar",
+        error: function (err) {
+            alert(err.responeJSON.msg);
+        },
+        success: function (data) {
+            $(".noticeBody").text(data.data[0].tips);
+            $(".vlogBody").attr("src", data.data[0].vlog);
+            let obj = data.data[0].recommended;
+            for (let key in obj) {
+                $(".recBody").append(`
+					<li><a href="` + obj[key] + `">` + key + `</a></li>
 			`);
-			}
+            }
+        }
+    });
 
-		}
-	});
+
 });
